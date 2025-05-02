7734439337f32147be65c0f849341d417866e93a
2811Yash <118837106+2811Yash@users.noreply.github.com>
2025-05-02 16:53:11 +0530

    Update route.ts

--- src/app/api/linkedin-posts/route.ts
+++ src/app/api/linkedin-posts/route.ts
@@ -4,7 +4,7 @@ import puppeteer from 'puppeteer';
 export async function GET(req: NextRequest) {
   try {
     const browser = await puppeteer.launch({
-      headless: 'false',
+      headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
